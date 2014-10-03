a3d9c9ad4f6c9b11c5e23dac54a5a8d16193397c
SAP HANA Academy <hanaacademy@sap.com>
2014-10-03 14:05:11 -0700

    Create HelloWorldServlet.java

---
+++ Java-development-on-HCP/Creating-a-Java-EE-6-Web-Profile-Application/src/Hello/HelloWorldServlet.java
@@ -0,0 +1,45 @@
+package hello;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.ejb.EJB;
+
+import test.HelloWorldBean;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private HelloWorldBean helloWorldBean;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println(helloWorldBean.sayHello());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
